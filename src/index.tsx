225ebe82c31dbc1b2e9531f089936f77e308a21b
Alex S. <simolex@yandex.ru>
2026-03-26 10:50:40 +0300

    Обновленный подход в маршрутизации

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './assets/scss/normalize.scss';
 import './assets/scss/style.scss';
 import { ToDoListPage } from './pages/ToDoListPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
-import { Header } from './components/Header/Header';
 import { Todo } from './models/todo-item';
 import { NotFound } from './pages/404';
 import { ItemDescription } from './pages/ItemDescription';
+import { Layout } from './layouts/Layout';
 
 
 const todos: Todo[] = [
@@ -34,21 +34,38 @@ const todos: Todo[] = [
   }
 ]
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage todos={todos} />
+      },
+      {
+        path: '/todo',
+        element: <ToDoListPage />
+      },
+      {
+        path: '/list/:id',
+        element: <ItemDescription todos={todos} />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+], { basename: '/app/' })
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage todos={todos} />}></Route>
-        <Route path='/list/:id' element={<ItemDescription todos={todos} />}></Route>
-        <Route path='/todo' element={<ToDoListPage />}></Route>
-        <Route path='*' element={<NotFound />}></Route>
-      </Routes>
-
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
